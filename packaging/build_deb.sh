7d76ce77c934633148018514b250d939765984bd
Daniel Pavel <daniel.pavel@gmail.com>
2013-01-05 17:40:06 +0200

    packaging updates for debian/ubunutu

--- packaging/build_deb.sh
+++ packaging/build_deb.sh
@@ -7,8 +7,8 @@ DEBIAN="$PWD/packaging/debian"
 RULES_D="$PWD/rules.d"
 
 BUILD_DIR="${TMPDIR:-/tmp}/$PWD"
-mkdir -m 0700 -p "$DIST"
-rm -rf "$DIST"/*
+mkdir -m 0700 -p "$BUILD_DIR"
+rm -rf "$BUILD_DIR"/*
 python setup.py sdist --dist-dir="$BUILD_DIR"
 
 cd "$BUILD_DIR"
@@ -20,18 +20,11 @@ mv "$S" solaar_$VERSION.orig.tar.gz
 
 cd solaar-*
 cp -a "$DEBIAN" .
-
 ls -1 "$RULES_D"/*.rules | while read rule; do
 	target=`basename "$rule"`
 	target=${target#??-}
 	target=${target%.rules}
 	cp -av "$rule" ./debian/solaar.$target.udev
 done
-
 debuild "$@"
-cd ..
-cp -au ./solaar_*.changes "$DIST"
-cp -au ./solaar_*.dsc "$DIST"
-cp -au ./solaar_*.debian.tar.gz "$DIST"
-cp -au ./solaar_*.orig.tar.gz "$DIST"
-cp -au ./solaar_*.deb "$DIST"
+cp -au ../solaar_* "$DIST"
--- packaging/upload_ppa.sh
+++ packaging/upload_ppa.sh
@@ -1,9 +1,10 @@
 #!/bin/sh
 
 cd `dirname "$0"`/..
-./tools/build_deb.sh -S -sa
+./packaging/build_deb.sh -S "$@"
 
 cd dist
 sed -e 's/UNRELEASED/precise/g' -i solaar_*_source.changes
 debsign --re-sign solaar_*_source.changes
-dput solaar-ppa solaar_*_source.changes
+dput -f solaar-ppa solaar_*_source.changes
+
