36b307eb496e96b58bbd958480490b362b9a912a
Daniel Pavel <daniel.pavel@gmail.com>
2013-05-21 00:47:47 +0300

    updated debian/ppa build scripts

--- packaging/build_deb.sh
+++ packaging/build_deb.sh
@@ -1,4 +1,4 @@
-#!/bin/sh
+#!/bin/sh -v
 
 set -e
 
@@ -6,10 +6,12 @@ if test ! -r "$HOME/.devscripts"; then
 	echo "$HOME/.descripts must exist"
 	exit 1
 fi
+. "$HOME/.devscripts"
 
 cd "$(dirname "$0")/.."
 DEBIAN_FILES="$PWD/packaging/debian"
 DIST="$PWD/dist/${DISTRIBUTION:=debian}"
+DIST_RELEASE=${DIST_RELEASE:-UNRELEASED}
 
 BUILD_DIR="${TMPDIR:-/tmp}/$DIST"
 rm -rf "$BUILD_DIR"
@@ -17,15 +19,39 @@ mkdir -m 0700 -p "$BUILD_DIR"
 python "setup.py" sdist --dist-dir="$BUILD_DIR" --formats=gztar
 
 cd "$BUILD_DIR"
-S=$(ls -1 solaar-*.tar.gz | head -n 1)
+S=$(ls -1t solaar-*.tar.gz | tail -n 1)
 test -r "$S"
 VERSION=${S#solaar-}
 VERSION=${VERSION%.tar.gz}
+
+LAST=$(head -n 1 "$DEBIAN_FILES/changelog" | grep -o ' ([0-9.-]*) ')
+LAST=${LAST# (}
+LAST=${LAST%) }
+LAST_VERSION=$(echo "$LAST" | cut -d- -f 1)
+LAST_BUILD=$(echo "$LAST" | cut -d- -f 2)
+
+if test -n "$BUILD_EXTRA"; then
+	BUILD_NUMBER=$LAST_BUILD
+elif dpkg --compare-versions "$VERSION" gt "$LAST_VERSION"; then
+	BUILD_NUMBER=1
+else
+	BUILD_NUMBER=$(($LAST_BUILD + 1))
+fi
+
 tar xfz "$S"
 mv "$S" solaar_$VERSION.orig.tar.gz
 
 cd solaar-$VERSION
 cp -a "$DEBIAN_FILES" .
+cat >debian/changelog <<_CHANGELOG
+solaar ($VERSION-$BUILD_NUMBER$BUILD_EXTRA) $DIST_RELEASE; urgency=low
+
+  * Debian packaging scripts, supports ubuntu ppa as well.
+
+ -- $DEBFULLNAME <$DEBMAIL>  $(date -R)
+
+_CHANGELOG
+test -z "$BUILD_EXTRA" && cp debian/changelog "$DEBIAN_FILES"/changelog
 
 test -n "$DEBIAN_FILES_EXTRA" && cp -a $DEBIAN_FILES_EXTRA/* debian/
 
--- packaging/build_ppa.sh
+++ packaging/build_ppa.sh
@@ -5,26 +5,10 @@ set -e
 cd "$(dirname "$0")/.."
 
 DISTRIBUTION=ubuntu
+DIST_RELEASE=precise
 DEBIAN_FILES_EXTRA="$PWD/packaging/ubuntu"
-
-. "$HOME/.devscripts"
-
-DEBIAN_CHANGELOG="$PWD/packaging/debian/changelog"
-PPA_CHANGELOG="$DEBIAN_FILES_EXTRA/changelog"
-
-latest="$(head -n 1 "$DEBIAN_CHANGELOG" | sed -e 's#(\([^)]*\))#(\1ppa1)#; s#UNRELEASED#precise#')"
-cat - "$DEBIAN_CHANGELOG" > "$PPA_CHANGELOG" <<_CHANGELOG
-$latest
-
-  * Customized debian/ for ubuntu launchpad ppa.
-
- -- $DEBFULLNAME <$DEBMAIL>  $(date -R)
-
-_CHANGELOG
-
+BUILD_EXTRA=ppa1
 DEBUILD_ARGS="-S -sa"
 . packaging/build_deb.sh
 
-rm -f "$PPA_CHANGELOG"
-
-#dput solaar-ppa solaar_*_source.changes
+dput solaar-snapshots-ppa solaar_*_source.changes
