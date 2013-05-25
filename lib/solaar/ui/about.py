078d003cfe397b15afb6ec64aa648d026a39c48c
Daniel Pavel <daniel.pavel@gmail.com>
2013-05-26 02:56:25 +0300

    about window icon was not displayed

--- lib/solaar/ui/about.py
+++ lib/solaar/ui/about.py
@@ -6,6 +6,7 @@ from __future__ import absolute_import, division, print_function, unicode_litera
 from gi.repository import Gtk
 
 from solaar import __version__, NAME
+from . import icons as _icons
 
 
 _dialog = None
@@ -14,11 +15,16 @@ _dialog = None
 def _create():
 	about = Gtk.AboutDialog()
 
+	icon_file = _icons.icon_file(NAME.lower())
+	if icon_file:
+		about.set_icon_from_file(icon_file)
+	else:
+		about.set_icon_name(NAME.lower())
+
 	about.set_program_name(NAME)
 	about.set_version(__version__)
 	about.set_comments('Shows status of devices connected\nto a Logitech Unifying Receiver.')
 
-	about.set_icon_name(NAME.lower())
 	about.set_logo_icon_name(NAME.lower() + '-logo')
 
 	about.set_copyright(b'\xC2\xA9'.decode('utf-8') + ' 2012-2013 Daniel Pavel')
@@ -31,7 +37,7 @@ def _create():
 						'Julien Gascard',
 						'Peter Wu http://www.lekensteyn.nl/logitech-unifying.html',
 						))
-		about.add_credit_section('Technical specifications\nprovided by', (
+		about.add_credit_section('Logitech documentation', (
 						'Julien Danjou http://julien.danjou.info/blog/2012/logitech-unifying-upower',
 						'Nestor Lopez Casado http://drive.google.com/folderview?id=0BxbRzx7vEV7eWmgwazJ3NUFfQ28',
 						))
