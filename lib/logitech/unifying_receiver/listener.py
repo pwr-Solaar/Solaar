31e134c1e040ee29faa1c993f17572e14e85a98e
Daniel Pavel <daniel.pavel@gmail.com>
2013-06-20 13:12:29 +0200

    don't wait so long on the first idle poll

--- lib/logitech/unifying_receiver/listener.py
+++ lib/logitech/unifying_receiver/listener.py
@@ -140,7 +140,7 @@ class EventsListener(_threading.Thread):
 		last_tick = 0
 		# the first idle read -- delay it a bit, and make sure to stagger
 		# idle reads for multiple receivers
-		idle_reads = _IDLE_READS * 2 + (ihandle % 5) * 4
+		idle_reads = _IDLE_READS + (ihandle % 5) * 2
 
 		while self._active:
 			if self._queued_notifications.empty():
