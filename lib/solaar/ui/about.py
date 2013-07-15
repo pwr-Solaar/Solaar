38c76393b1addb3e1a85774331ec5ba04e19b97e
Daniel Pavel <daniel.pavel@gmail.com>
2013-07-15 17:54:42 +0200

    added copyright notices to all source files

--- lib/solaar/ui/about.py
+++ lib/solaar/ui/about.py
@@ -1,6 +1,22 @@
+# -*- python-mode -*-
 # -*- coding: UTF-8 -*-
-#
-#
+
+## Copyright (C) 2012-2013  Daniel Pavel
+##
+## This program is free software; you can redistribute it and/or modify
+## it under the terms of the GNU General Public License as published by
+## the Free Software Foundation; either version 2 of the License, or
+## (at your option) any later version.
+##
+## This program is distributed in the hope that it will be useful,
+## but WITHOUT ANY WARRANTY; without even the implied warranty of
+## MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+## GNU General Public License for more details.
+##
+## You should have received a copy of the GNU General Public License along
+## with this program; if not, write to the Free Software Foundation, Inc.,
+## 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA.
+
 from __future__ import absolute_import, division, print_function, unicode_literals
 
 from gi.repository import Gtk
