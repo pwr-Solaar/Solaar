acd8fc77ca9ba38f6a41735bc6e7ebb044a56349
Peter F. Patel-Schneider <pfpschneider@gmail.com>
2022-12-07 15:13:38 -0500

    tools: remove unnecessary clean.sh

--- tools/clean.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/sh
-
-cd "$(dirname "$0")/.."
-
-find . -type f -name '*.py[co]' -delete
-find . -type d -name '__pycache__' -delete
-
-/bin/rm --force po/*~
-/bin/rm --force --recursive share/locale/
