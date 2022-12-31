1bea3606002796a74e5c6e9f37e18290503f751f
Peter F. Patel-Schneider <pfpschneider@gmail.com>
2022-12-31 08:39:49 -0500

    device: correctly determine whether to ping with a long HID++ message

--- lib/logitech_receiver/device.py
+++ lib/logitech_receiver/device.py
@@ -442,7 +442,7 @@ class Device:
 
     def ping(self):
         """Checks if the device is online, returns True of False"""
-        long = self.bluetooth or self._protocol is not None and self._protocol >= 2.0
+        long = self.bluetooth or self.hidpp_short is False or self._protocol is not None and self._protocol >= 2.0
         protocol = _base.ping(self.handle or self.receiver.handle, self.number, long_message=long)
         self.online = protocol is not None
         if protocol:
