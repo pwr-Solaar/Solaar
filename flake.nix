aa2044d8e902fb7691bf31b021fa9f2e96b0293c
Svenum <s.ziegler@holypenguin.net>
2023-09-18 17:36:01 +0200

    add flake

---
+++ flake.nix
@@ -0,0 +1,21 @@
+{
+  description = "Solaar is Open Source Logitech Driver for Linux";
+
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-22.11";
+
+    flake-compat = {
+      url = "github:edolstra/flake-compat";
+      flake = false;
+    }
+  };
+
+  outputs = { self, nixpkgs, flake-compat }: {
+
+    packages.x86_64-linux.default = (
+      import nixpkgs {
+        currentSystem = "x86_64-linux";
+        localSystem = "x86_64-linux";
+      }).pkgs.callPackage ./nix/solaar {};
+  };
+}
