4ff2eef146ca82554c9c001ee531001080f57a61
Svenum <s.ziegler@holypenguin.net>
2023-09-18 18:02:42 +0200

    updat package

--- nix/solaar/default.nix
+++ nix/solaar/default.nix
@@ -1,2 +1,23 @@
-{ pkgs ? import <nixpkgs> {} }:
-pkgs.callPackage ./derivation.nix {}
+{stdenv,
+lib,
+builtins,
+python3
+}:
+
+stdenv.mkDerivation rec{
+  pname = "solaar";
+  version = builtins.readFile "../../lib/solaar/version";
+
+  src = "../../";
+
+  builtInputs = [
+    python3
+  ];
+
+  installPhase = ''
+    cp ${src}/bin/solaar ${out}/bin/solaar
+    ln -s ${out}/bin/solaar ${out}/bin/solaar-cli
+
+    cp -r ${src}/lib ${out}/lib
+  '';
+}
