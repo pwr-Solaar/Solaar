fea0c334b0317ee3ec5076fab70783979061ea7f
Cliff Kang <kangcliff@gmail.com>
2023-11-27 01:03:03 +0800

    Fix for styling mistakes.

--- lib/hidapi/win_impl/__init__.py
+++ lib/hidapi/win_impl/__init__.py
@@ -1,1 +1,12 @@
-from .impl import *
+from .impl import close,\
+                enumerate,\
+                find_paired_node,\
+                find_paired_node_wpid,\
+                get_manufacturer,\
+                get_product,\
+                get_serial,\
+                monitor_glib,\
+                open,\
+                open_path,\
+                read,\
+                write
