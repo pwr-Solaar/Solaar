90b0db6c3ba1ad5360dbc7d36989ced5efc6d75f
Peter F. Patel-Schneider <pfpschneider@gmail.com>
2024-05-22 21:09:08 -0400

    device: don't ping device when getting name or codename

--- lib/logitech_receiver/device.py
+++ lib/logitech_receiver/device.py
@@ -178,8 +178,6 @@ class Device:
     @property
     def codename(self):
         if not self._codename:
-            if not self.online:  # be very defensive
-                self.ping()
             if self.online and self.protocol >= 2.0:
                 self._codename = _hidpp20.get_friendly_name(self)
                 if not self._codename:
@@ -189,20 +187,15 @@ class Device:
                 if codename:
                     self._codename = codename
                 elif self.protocol < 2.0:
-                    self._codename = "? (%s)" % (self.wpid or hex(self.product_id)[2:].upper())
-        return self._codename or "?? (%s)" % (self.wpid or hex(self.product_id)[2:].upper())
+                    self._codename = "? (%s)" % (self.wpid or self.product_id)
+        return self._codename or "?? (%s)" % (self.wpid or self.product_id)
 
     @property
     def name(self):
         if not self._name:
-            if not self.online:  # be very defensive
-                try:
-                    self.ping()
-                except exceptions.NoSuchDevice:
-                    pass
             if self.online and self.protocol >= 2.0:
                 self._name = _hidpp20.get_name(self)
-        return self._name or self._codename or ("Unknown device %s" % (self.wpid or hex(self.product_id)[2:].upper()))
+        return self._name or self._codename or "Unknown device %s" % (self.wpid or self.product_id)
 
     def get_ids(self):
         ids = _hidpp20.get_ids(self)
