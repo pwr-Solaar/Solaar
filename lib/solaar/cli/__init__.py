b855ae16458106eaf5e069fe4db8ddefd56009ae
MattHag <16444067+MattHag@users.noreply.github.com>
2024-06-24 22:47:08 +0200

    Remove import as _ in cli init
    Related #2273

--- lib/solaar/cli/__init__.py
+++ lib/solaar/cli/__init__.py
@@ -14,18 +14,17 @@
 ## with this program; if not, write to the Free Software Foundation, Inc.,
 ## 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA.
 
-import argparse as _argparse
+import argparse
 import logging
-import sys as _sys
+import sys
 
 from importlib import import_module
 from traceback import extract_tb
 from traceback import format_exc
 
-import logitech_receiver.device as _device
-import logitech_receiver.receiver as _receiver
-
 from logitech_receiver import base
+from logitech_receiver import device
+from logitech_receiver import receiver
 
 from solaar import NAME
 
@@ -33,7 +32,7 @@ logger = logging.getLogger(__name__)
 
 
 def _create_parser():
-    parser = _argparse.ArgumentParser(
+    parser = argparse.ArgumentParser(
         prog=NAME.lower(), add_help=False, epilog=f"For details on individual actions, run `{NAME.lower()} <action> --help`."
     )
     subparsers = parser.add_subparsers(title="actions", help="optional action to perform")
@@ -107,14 +106,14 @@ def _receivers(dev_path=None):
         if dev_path is not None and dev_path != dev_info.path:
             continue
         try:
-            r = _receiver.ReceiverFactory.create_receiver(dev_info)
+            r = receiver.ReceiverFactory.create_receiver(dev_info)
             if logger.isEnabledFor(logging.DEBUG):
                 logger.debug("[%s] => %s", dev_info.path, r)
             if r:
                 yield r
         except Exception as e:
             logger.exception("opening " + str(dev_info))
-            _sys.exit(f"{NAME.lower()}: error: {str(e)}")
+            sys.exit(f"{NAME.lower()}: error: {str(e)}")
 
 
 def _receivers_and_devices(dev_path=None):
@@ -123,9 +122,9 @@ def _receivers_and_devices(dev_path=None):
             continue
         try:
             if dev_info.isDevice:
-                d = _device.DeviceFactory.create_device(base, dev_info)
+                d = device.DeviceFactory.create_device(base, dev_info)
             else:
-                d = _receiver.ReceiverFactory.create_receiver(dev_info)
+                d = receiver.ReceiverFactory.create_receiver(dev_info)
 
             if logger.isEnabledFor(logging.DEBUG):
                 logger.debug("[%s] => %s", dev_info.path, d)
@@ -133,7 +132,7 @@ def _receivers_and_devices(dev_path=None):
                 yield d
         except Exception as e:
             logger.exception("opening " + str(dev_info))
-            _sys.exit(f"{NAME.lower()}: error: {str(e)}")
+            sys.exit(f"{NAME.lower()}: error: {str(e)}")
 
 
 def _find_receiver(receivers, name):
@@ -197,9 +196,9 @@ def run(cli_args=None, hidraw_path=None):
         # Python 3 has an undocumented 'feature' that breaks parsing empty args
         # http://bugs.python.org/issue16308
         if "cmd" not in args:
-            _cli_parser.print_usage(_sys.stderr)
-            _sys.stderr.write(f"{NAME.lower()}: error: too few arguments\n")
-            _sys.exit(2)
+            _cli_parser.print_usage(sys.stderr)
+            sys.stderr.write(f"{NAME.lower()}: error: too few arguments\n")
+            sys.exit(2)
         action = args.action
     assert action in actions
 
@@ -215,7 +214,7 @@ def run(cli_args=None, hidraw_path=None):
         m = import_module("." + action, package=__name__)
         m.run(c, args, _find_receiver, _find_device)
     except AssertionError:
-        tb_last = extract_tb(_sys.exc_info()[2])[-1]
-        _sys.exit(f"{NAME.lower()}: assertion failed: {tb_last[0]} line {int(tb_last[1])}")
+        tb_last = extract_tb(sys.exc_info()[2])[-1]
+        sys.exit(f"{NAME.lower()}: assertion failed: {tb_last[0]} line {int(tb_last[1])}")
     except Exception:
-        _sys.exit(f"{NAME.lower()}: error: {format_exc()}")
+        sys.exit(f"{NAME.lower()}: error: {format_exc()}")
