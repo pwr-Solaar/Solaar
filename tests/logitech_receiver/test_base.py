02123380979c110971b3a7c377d5898f5edce22d
MattHag <16444067+MattHag@users.noreply.github.com>
2024-09-15 18:52:22 +0200

    Replace global sw_id with function call
    Add test for it.

--- tests/logitech_receiver/test_base.py
+++ tests/logitech_receiver/test_base.py
@@ -22,3 +22,11 @@ def test_product_information(usb_id, expected_name, expected_receiver_kind):
 
     if expected_receiver_kind:
         assert res["receiver_kind"] == expected_receiver_kind
+
+
+def test_get_next_sw_id():
+    res1 = base._get_next_sw_id()
+    res2 = base._get_next_sw_id()
+
+    assert res1 == 2
+    assert res2 == 3
