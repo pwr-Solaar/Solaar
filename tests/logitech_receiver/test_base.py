57438b8b45a398dd0924b8fc30056a66e4f6c16d
MattHag <16444067+MattHag@users.noreply.github.com>
2024-09-28 01:14:32 +0200

    base_usb: Add external interface
    Clean up, type hint and tests base_usb and related modules.

--- tests/logitech_receiver/test_base.py
+++ tests/logitech_receiver/test_base.py
@@ -24,6 +24,28 @@ def test_product_information(usb_id, expected_name, expected_receiver_kind):
         assert res["receiver_kind"] == expected_receiver_kind
 
 
+def test_filter_receivers_known():
+    bus_id = 2
+    vendor_id = 0x046D
+    product_id = 0xC548
+
+    receiver_info = base.filter_receivers(bus_id, vendor_id, product_id)
+
+    assert receiver_info["name"] == "Bolt Receiver"
+    assert receiver_info["receiver_kind"] == "bolt"
+
+
+def test_filter_receivers_unknown():
+    bus_id = 1
+    vendor_id = 0x046D
+    product_id = 0xC500
+
+    receiver_info = base.filter_receivers(bus_id, vendor_id, product_id)
+
+    assert receiver_info["bus_id"] == bus_id
+    assert receiver_info["product_id"] == product_id
+
+
 def test_get_next_sw_id():
     res1 = base._get_next_sw_id()
     res2 = base._get_next_sw_id()
