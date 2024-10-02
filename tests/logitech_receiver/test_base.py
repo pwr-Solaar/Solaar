9e10039eac1f3f2133418e4d3ce1a6122f6daae7
MattHag <16444067+MattHag@users.noreply.github.com>
2024-10-02 22:07:28 +0200

    base: Add more unit tests
    Make internal functions private.

--- tests/logitech_receiver/test_base.py
+++ tests/logitech_receiver/test_base.py
@@ -29,7 +29,7 @@ def test_filter_receivers_known():
     vendor_id = 0x046D
     product_id = 0xC548
 
-    receiver_info = base.filter_receivers(bus_id, vendor_id, product_id)
+    receiver_info = base._filter_receivers(bus_id, vendor_id, product_id)
 
     assert receiver_info["name"] == "Bolt Receiver"
     assert receiver_info["receiver_kind"] == "bolt"
@@ -40,22 +40,28 @@ def test_filter_receivers_unknown():
     vendor_id = 0x046D
     product_id = 0xC500
 
-    receiver_info = base.filter_receivers(bus_id, vendor_id, product_id)
+    receiver_info = base._filter_receivers(bus_id, vendor_id, product_id)
 
     assert receiver_info["bus_id"] == bus_id
     assert receiver_info["product_id"] == product_id
 
 
 @pytest.mark.parametrize(
-    "hidpp_short, hidpp_long",
-    [(True, False), (False, True), (False, False)],
+    "product_id, hidpp_short, hidpp_long",
+    [
+        (0xC548, True, False),
+        (0xC07E, True, False),
+        (0xC07E, False, True),
+        (0xA07E, False, True),
+        (0xA07E, None, None),
+        (0xA07C, False, False),
+    ],
 )
-def test_filter_products_of_interest(hidpp_short, hidpp_long):
+def test_filter_products_of_interest(product_id, hidpp_short, hidpp_long):
     bus_id = 3
     vendor_id = 0x046D
-    product_id = 0xC07E
 
-    receiver_info = base.filter_products_of_interest(
+    receiver_info = base._filter_products_of_interest(
         bus_id,
         vendor_id,
         product_id,
@@ -63,8 +69,40 @@ def test_filter_products_of_interest(hidpp_short, hidpp_long):
         hidpp_long=hidpp_long,
     )
 
-    assert receiver_info["bus_id"] == bus_id
-    assert receiver_info["product_id"] == product_id
+    if not hidpp_short and not hidpp_long:
+        assert receiver_info is None
+    else:
+        assert isinstance(receiver_info["vendor_id"], int)
+        assert receiver_info["product_id"] == product_id
+
+
+@pytest.mark.parametrize(
+    "report_id, sub_id, address, valid_notification",
+    [
+        (0x1, 0x72, 0x57, True),
+        (0x1, 0x40, 0x63, True),
+        (0x1, 0x40, 0x71, True),
+        (0x1, 0x80, 0x71, False),
+        (0x1, 0x00, 0x70, False),
+        (0x20, 0x09, 0x71, False),
+        (0x1, 0x37, 0x71, False),
+    ],
+)
+def test_make_notification(report_id, sub_id, address, valid_notification):
+    devnumber = 123
+    data = bytes([sub_id, address, 0x02, 0x03, 0x04])
+
+    result = base.make_notification(report_id, devnumber, data)
+
+    if valid_notification:
+        assert isinstance(result, base.HIDPPNotification)
+        assert result.report_id == report_id
+        assert result.devnumber == devnumber
+        assert result.sub_id == sub_id
+        assert result.address == address
+        assert result.data == bytes([0x02, 0x03, 0x04])
+    else:
+        assert result is None
 
 
 def test_get_next_sw_id():
