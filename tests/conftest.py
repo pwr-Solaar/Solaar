8e631291c1926223d5c9fb3bbc29ff947d0cada4
MattHag <16444067+MattHag@users.noreply.github.com>
2025-07-12 02:16:14 +0200

    Implement HID++ 1.0 fake device for some basic requests
    The fake device is a simplified, working implementation of a HID++ 1.0
    device, that behaves similar to the real hardware. This allows tests to
    be closer to the real world.
    
    Related #2905

--- tests/conftest.py
+++ tests/conftest.py
@@ -1,6 +1,13 @@
 import pytest
 
+from fakes import hidpp10_device
+
 from .fakes import hidpp20_device
+
+
+@pytest.fixture
+def device_hidpp10():
+    yield hidpp10_device.Hidpp10Device()
 
 
 @pytest.fixture
---
+++ tests/logitech_receiver/test_hidpp10.py
@@ -0,0 +1,27 @@
+from logitech_receiver import hidpp10
+from logitech_receiver import hidpp10_constants
+from logitech_receiver.hidpp10_constants import Registers
+
+_hidpp10 = hidpp10.Hidpp10()
+
+
+def test_read_register(device_hidpp10):
+    result = hidpp10.read_register(
+        device_hidpp10,
+        register=Registers.BATTERY_STATUS,
+    )
+
+    assert result == bytes.fromhex("666666")
+
+
+def test_set_notification_flags(mocker, device_hidpp10):
+    spy_request = mocker.spy(device_hidpp10, "request")
+
+    result = _hidpp10.set_notification_flags(
+        device_hidpp10,
+        hidpp10_constants.NotificationFlag.BATTERY_STATUS,
+        hidpp10_constants.NotificationFlag.WIRELESS,
+    )
+
+    spy_request.assert_called_once_with(0x8000 | Registers.NOTIFICATIONS, b"\x10\x01\x00")
+    assert result is not None
