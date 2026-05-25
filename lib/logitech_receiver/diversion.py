pwr-Solaar/Solaar

commit 46366b2430126954738ae5537981bbddca3ef9f7

Fix warnings from automatic code inspections
Warnings found by automatic code inspection and partially tackled
- Drop distuitls inf favour of setuptools
- Replace deprecated pyudev.Device.from_device_number
- Remove unnecessary brackets
- Avoid access to private variables etc.
- Shadows built-in name
- Line length >120 characters
- Not a module level variable
- Simplify clause
and more

lib/logitech_receiver/diversion.py
@@ -26,6 +26,7 @@ import subprocess
 import sys
 import time
 
+from typing import Any
 from typing import Dict
 from typing import Tuple
 
@@ -113,9 +114,17 @@ except Exception:
 # Globals
 xtest_available = True  # Xtest might be available
 xdisplay = None
+
+
 Xkbdisplay = None  # xkb might be available
+X11Lib = None
+
 modifier_keycodes = []
 XkbUseCoreKbd = 0x100
+NET_ACTIVE_WINDOW = None
+NET_WM_PID = None
+WM_CLASS = None
+
 
 udevice = None
 
@@ -187,7 +196,10 @@ def gnome_dbus_interface_setup():
         remote_object = bus.get_object("org.gnome.Shell", "/io/github/pwr_solaar/solaar")
         _dbus_interface = dbus.Interface(remote_object, "io.github.pwr_solaar.solaar")
     except dbus.exceptions.DBusException:
-        logger.warning("Solaar Gnome extension not installed - some rule capabilities inoperable", exc_info=sys.exc_info())
+        logger.warning(
+            "Solaar Gnome extension not installed - some rule capabilities inoperable",
+            exc_info=sys.exc_info(),
+        )
         _dbus_interface = False
     return _dbus_interface
 
@@ -228,7 +240,10 @@ if evdev:
     for _, evcode in buttons.values():
         if evcode:
             key_events.append(evcode)
-    devicecap = {evdev.ecodes.EV_KEY: key_events, evdev.ecodes.EV_REL: [evdev.ecodes.REL_WHEEL, evdev.ecodes.REL_HWHEEL]}
+    devicecap = {
+        evdev.ecodes.EV_KEY: key_events,
+        evdev.ecodes.EV_REL: [evdev.ecodes.REL_WHEEL, evdev.ecodes.REL_HWHEEL],
+    }
 else:
     # Just mock these since they won't be useful without evdev anyway
     buttons = {}
@@ -283,9 +298,9 @@ def xy_direction(_x, _y):
     y = round(_y / m)
     if x < 0 and y < 0:
         return "Mouse Up-left"
-    elif x > 0 and y < 0:
+    elif x > 0 > y:
         return "Mouse Up-right"
-    elif x < 0 and y > 0:
+    elif x < 0 < y:
         return "Mouse Down-left"
     elif x > 0 and y > 0:
         return "Mouse Down-right"
@@ -456,7 +471,7 @@ TESTS = {
     "crown_tap": [lambda f, r, d, a: f == FEATURE.CROWN and r == 0 and d[5] == 0x01 and d[5], False],
     "crown_start_press": [lambda f, r, d, a: f == FEATURE.CROWN and r == 0 and d[6] == 0x01 and d[6], False],
     "crown_end_press": [lambda f, r, d, a: f == FEATURE.CROWN and r == 0 and d[6] == 0x05 and d[6], False],
-    "crown_pressed": [lambda f, r, d, a: f == FEATURE.CROWN and r == 0 and d[6] >= 0x01 and d[6] <= 0x04 and d[6], False],
+    "crown_pressed": [lambda f, r, d, a: f == FEATURE.CROWN and r == 0 and 0x01 <= d[6] <= 0x04 and d[6], False],
     "thumb_wheel_up": [thumb_wheel_up, True],
     "thumb_wheel_down": [thumb_wheel_down, True],
     "lowres_wheel_up": [
@@ -488,7 +503,7 @@ MOUSE_GESTURE_TESTS = {
     "mouse-noop": [],
 }
 
-COMPONENTS = {}
+# COMPONENTS = {}
 
 
 class RuleComponent:
@@ -503,6 +518,17 @@ class RuleComponent:
         return Condition()
 
 
+def _evaluate(components, feature, notification, device, result) -> Any:
+    res = True
+    for component in components:
+        res = component.evaluate(feature, notification, device, result)
+        if not isinstance(component, Action) and res is None:
+            return None
+        if isinstance(component, Condition) and not res:
+            return res
+    return res
+
+
 class Rule(RuleComponent):
     def __init__(self, args, source=None, warn=True):
         self.components = [self.compile(a) for a in args]
@@ -515,14 +541,7 @@ class Rule(RuleComponent):
     def evaluate(self, feature, notification, device, last_result):
         if logger.isEnabledFor(logging.DEBUG):
             logger.debug("evaluate rule: %s", self)
-        result = True
-        for component in self.components:
-            result = component.evaluate(feature, notification, device, result)
-            if not isinstance(component, Action) and result is None:
-                return None
-            if isinstance(component, Condition) and not result:
-                return result
-        return result
+        return _evaluate(self.components, feature, notification, device, True)
 
     def once(self, feature, notification, device, last_result):
         self.evaluate(feature, notification, device, last_result)
@@ -598,14 +617,7 @@ class And(Condition):
     def evaluate(self, feature, notification, device, last_result):
         if logger.isEnabledFor(logging.DEBUG):
             logger.debug("evaluate condition: %s", self)
-        result = True
-        for component in self.components:
-            result = component.evaluate(feature, notification, device, last_result)
-            if not isinstance(component, Action) and result is None:
-                return None
-            if isinstance(component, Condition) and not result:
-                return result
-        return result
+        return _evaluate(self.components, feature, notification, device, last_result)
 
     def data(self):
         return {"And": [c.data() for c in self.components]}
@@ -663,7 +675,8 @@ class Process(Condition):
         if (not wayland and not x11_setup()) or (wayland and not gnome_dbus_interface_setup()):
             if warn:
                 logger.warning(
-                    "rules can only access active process in X11 or in Wayland under GNOME with Solaar Gnome extension - %s",
+                    "rules can only access active process in X11 or in Wayland under GNOME with Solaar Gnome "
+                    "extension - %s",
                     self,
                 )
         if not isinstance(process, str):
@@ -1218,7 +1231,13 @@ class KeyPress(Action):
         if gkeymap:
             current = gkeymap.get_modifier_state()
             if logger.isEnabledFor(logging.INFO):
-                logger.info("KeyPress action: %s %s, group %s, modifiers %s", self.key_names, self.action, kbdgroup(), current)
+                logger.info(
+                    "KeyPress action: %s %s, group %s, modifiers %s",
+                    self.key_names,
+                    self.action,
+                    kbdgroup(),
+                    current,
+                )
             if self.action != RELEASE:
                 self.keyDown(self.key_symbols, current)
             if self.action != DEPRESS:
@@ -1287,7 +1306,10 @@ class MouseClick(Action):
             if count in [CLICK, DEPRESS, RELEASE]:
                 self.count = count
             elif warn:
-                logger.warning("rule MouseClick action: argument %s should be an integer or CLICK, PRESS, or RELEASE", count)
+                logger.warning(
+                    "rule MouseClick action: argument %s should be an integer or CLICK, PRESS, or RELEASE",
+                    count,
+                )
                 self.count = 1
 
     def __str__(self):
@@ -1332,7 +1354,12 @@ class Set(Action):
             return None
         args = setting.acceptable(self.args[2:], setting.read())
         if args is None:
-            logger.warning("Set Action: invalid args %s for setting %s of %s", self.args[2:], self.args[1], self.args[0])
+            logger.warning(
+                "Set Action: invalid args %s for setting %s of %s",
+                self.args[2:],
+                self.args[1],
+                self.args[0],
+            )
             return None
         if len(args) > 1:
             setting.write_key_value(args[0], args[1])
@@ -1432,18 +1459,17 @@ COMPONENTS = {
     "Later": Later,
 }
 
-built_in_rules = Rule([])
-if True:
-    built_in_rules = Rule(
-        [
-            {
-                "Rule": [  # Implement problematic keys for Craft and MX Master
-                    {"Rule": [{"Key": ["Brightness Down", "pressed"]}, {"KeyPress": "XF86_MonBrightnessDown"}]},
-                    {"Rule": [{"Key": ["Brightness Up", "pressed"]}, {"KeyPress": "XF86_MonBrightnessUp"}]},
-                ]
-            },
-        ]
-    )
+
+built_in_rules = Rule(
+    [
+        {
+            "Rule": [  # Implement problematic keys for Craft and MX Master
+                {"Rule": [{"Key": ["Brightness Down", "pressed"]}, {"KeyPress": "XF86_MonBrightnessDown"}]},
+                {"Rule": [{"Key": ["Brightness Up", "pressed"]}, {"KeyPress": "XF86_MonBrightnessUp"}]},
+            ]
+        },
+    ]
+)
 
 
 def key_is_down(key):

bin/solaar
@@ -25,6 +25,7 @@ def init_paths():
     import sys
 
     # Python 3 might have problems converting back to UTF-8 in case of Unicode surrogates
+    decoded_path = None
     try:
         decoded_path = sys.path[0]
         sys.path[0].encode(sys.getfilesystemencoding())

lib/hid_parser/__init__.py
@@ -560,7 +560,8 @@ class ArrayItem(MainItem):
                 )
                 continue
 
-            if usage in self._usages and all(usage_type not in self._INCOMPATIBLE_TYPES for usage_type in usage.usage_types):
+            not_incompatible_type = all(usage_type not in self._INCOMPATIBLE_TYPES for usage_type in usage.usage_types)
+            if usage in self._usages and not_incompatible_type:
                 usage_values[usage] = UsageValue(self, True)
 
         return usage_values
@@ -820,14 +821,28 @@ class ReportDescriptor:
                     if data is None:
                         raise InvalidReportDescriptor("Invalid output item")
                     self._append_items(
-                        offset_output, self._output, report_id, report_count, report_size, usages, data, {**glob, **local}
+                        offset_output,
+                        self._output,
+                        report_id,
+                        report_count,
+                        report_size,
+                        usages,
+                        data,
+                        {**glob, **local},
                     )
 
                 elif tag == TagMain.FEATURE:
                     if data is None:
                         raise InvalidReportDescriptor("Invalid feature item")
                     self._append_items(
-                        offset_feature, self._feature, report_id, report_count, report_size, usages, data, {**glob, **local}
+                        offset_feature,
+                        self._feature,
+                        report_id,
+                        report_count,
+                        report_size,
+                        usages,
+                        data,
+                        {**glob, **local},
                     )
 
                 # clear local

lib/hidapi/common.py
@@ -1,18 +1,20 @@
+from __future__ import annotations
+
 import dataclasses
 
 
 @dataclasses.dataclass
 class DeviceInfo:
     path: str
-    bus_id: str
+    bus_id: str | None
     vendor_id: str
     product_id: str
-    interface: str
-    driver: str
-    manufacturer: str
-    product: str
-    serial: str
-    release: str
+    interface: str | None
+    driver: str | None
+    manufacturer: str | None
+    product: str | None
+    serial: str | None
+    release: str | None
     isDevice: bool
-    hidpp_short: str
-    hidpp_long: str
+    hidpp_short: str | None
+    hidpp_long: str | None

lib/hidapi/hidapi_impl.py
@@ -33,6 +33,7 @@ import typing
 
 from threading import Thread
 from time import sleep
+from typing import Callable
 
 from hidapi.common import DeviceInfo
 
@@ -203,6 +204,7 @@ class _DeviceMonitor(Thread):
     def __init__(self, device_callback, polling_delay=5.0):
         self.device_callback = device_callback
         self.polling_delay = polling_delay
+        self.prev_devices = None
         # daemon threads are automatically killed when main thread exits
         super().__init__(daemon=True)
 
@@ -259,7 +261,12 @@ def _match(action, device, filterfn):
 
     if logger.isEnabledFor(logging.INFO):
         logger.info(
-            "Found device BID %s VID %04X PID %04X HID++ %s %s", bus_id, vid, pid, device["hidpp_short"], device["hidpp_long"]
+            "Found device BID %s VID %04X PID %04X HID++ %s %s",
+            bus_id,
+            vid,
+            pid,
+            device["hidpp_short"],
+            device["hidpp_long"],
         )
 
     if not device["hidpp_short"] and not device["hidpp_long"]:
@@ -317,7 +324,7 @@ def find_paired_node_wpid(receiver_path: str, index: int):
     return None
 
 
-def monitor_glib(glib: GLib, callback, filterfn):
+def monitor_glib(glib: GLib, callback: Callable, filterfn: Callable):
     """Monitor GLib.
 
     Parameters
@@ -452,7 +459,6 @@ def read(device_handle, bytes_count, timeout_ms=None):
 
     if bytes_read < 0:
         raise HIDError(_hidapi.hid_error(device_handle))
-        return None
 
     return data.raw[:bytes_read]
 

lib/hidapi/udev_impl.py
@@ -36,6 +36,7 @@ import warnings
 from select import select
 from time import sleep
 from time import time
+from typing import Callable
 
 import pyudev
 
@@ -114,7 +115,12 @@ def _match(action, device, filter_func: typing.Callable[[int, int, int, bool, bo
         hidpp_short = None
         hidpp_long = None
         logger.info(
-            "Report Descriptor not processed for DEVICE %s BID %s VID %s PID %s: %s", device.device_node, bid, vid, pid, e
+            "Report Descriptor not processed for DEVICE %s BID %s VID %s PID %s: %s",
+            device.device_node,
+            bid,
+            vid,
+            pid,
+            e,
         )
 
     filtered_result = filter_func(int(bid, 16), int(vid, 16), int(pid, 16), hidpp_short, hidpp_long)
@@ -222,7 +228,7 @@ def find_paired_node_wpid(receiver_path, index):
     return None
 
 
-def monitor_glib(glib: GLib, callback, filterfn):
+def monitor_glib(glib: GLib, callback: Callable, filterfn: typing.Callable):
     """Monitor GLib.
 
     Parameters
@@ -453,7 +459,7 @@ def get_indexed_string(device_handle, index):
     assert device_handle
     stat = os.fstat(device_handle)
     try:
-        dev = pyudev.Device.from_device_number(pyudev.Context(), "char", stat.st_rdev)
+        dev = pyudev.Devices.from_device_number(pyudev.Context(), "char", stat.st_rdev)
     except (pyudev.DeviceNotFoundError, ValueError):
         return None
 

lib/logitech_receiver/base.py
@@ -29,6 +29,7 @@ from contextlib import contextmanager
 from random import getrandbits
 from time import time
 from typing import Any
+from typing import Callable
 
 import gi
 
@@ -53,7 +54,7 @@ else:
 logger = logging.getLogger(__name__)
 
 
-_SHORT_MESSAGE_SIZE = 7
+SHORT_MESSAGE_SIZE = 7
 _LONG_MESSAGE_SIZE = 20
 _MEDIUM_MESSAGE_SIZE = 15
 _MAX_READ_SIZE = 32
@@ -138,7 +139,7 @@ def _match(record: dict[str, Any], bus_id: int, vendor_id: int, product_id: int)
 
 
 def filter_receivers(
-    bus_id: int, vendor_id: int, product_id: int, hidpp_short: bool = False, hidpp_long: bool = False
+    bus_id: int, vendor_id: int, product_id: int, _hidpp_short: bool = False, _hidpp_long: bool = False
 ) -> dict[str, Any]:
     """Check that this product is a Logitech receiver.
 
@@ -184,7 +185,7 @@ def receivers_and_devices():
     yield from hidapi.enumerate(filter_products_of_interest)
 
 
-def notify_on_receivers_glib(glib: GLib, callback):
+def notify_on_receivers_glib(glib: GLib, callback: Callable):
     """Watch for matching devices and notifies the callback on the GLib thread.
 
     Parameters
@@ -254,7 +255,7 @@ def write(handle, devnumber, data, long_message=False):
     assert data is not None
     assert isinstance(data, bytes), (repr(data), type(data))
 
-    if long_message or len(data) > _SHORT_MESSAGE_SIZE - 2 or data[:1] == b"\x82":
+    if long_message or len(data) > SHORT_MESSAGE_SIZE - 2 or data[:1] == b"\x82":
         wdata = struct.pack("!BB18s", HIDPP_LONG_MESSAGE_ID, devnumber, data)
     else:
         wdata = struct.pack("!BB5s", HIDPP_SHORT_MESSAGE_ID, devnumber, data)
@@ -303,7 +304,7 @@ def is_relevant_message(data: bytes) -> bool:
 
     # mapping from report_id to message length
     report_lengths = {
-        HIDPP_SHORT_MESSAGE_ID: _SHORT_MESSAGE_SIZE,
+        HIDPP_SHORT_MESSAGE_ID: SHORT_MESSAGE_SIZE,
         HIDPP_LONG_MESSAGE_ID: _LONG_MESSAGE_SIZE,
         DJ_MESSAGE_ID: _MEDIUM_MESSAGE_SIZE,
         0x21: _MAX_READ_SIZE,
@@ -344,7 +345,12 @@ def _read(handle, timeout):
             report_id != DJ_MESSAGE_ID or ord(data[2:3]) > 0x10
         ):  # ignore DJ input messages
             logger.debug(
-                "(%s) => r[%02X %02X %s %s]", handle, report_id, devnumber, common.strhex(data[2:4]), common.strhex(data[4:])
+                "(%s) => r[%02X %02X %s %s]",
+                handle,
+                report_id,
+                devnumber,
+                common.strhex(data[2:4]),
+                common.strhex(data[4:]),
             )
 
         return report_id, devnumber, data[2:]
@@ -554,7 +560,12 @@ def request(
                             error,
                             hidpp20_constants.ERROR[error],
                         )
-                        raise exceptions.FeatureCallError(number=devnumber, request=request_id, error=error, params=params)
+                        raise exceptions.FeatureCallError(
+                            number=devnumber,
+                            request=request_id,
+                            error=error,
+                            params=params,
+                        )
 
                     if reply_data[:2] == request_data[:2]:
                         if devnumber == 0xFF:
@@ -628,7 +639,7 @@ def ping(handle, devnumber, long_message: bool = False):
                         and reply_data[1:3] == request_data[:2]
                     ):  # error response
                         error = ord(reply_data[3:4])
-                        if error == hidpp10_constants.ERROR.invalid_SubID__command:  # a valid reply from a HID++ 1.0 device
+                        if error == hidpp10_constants.ERROR.invalid_SubID__command:  # valid reply from HID++ 1.0 device
                             return 1.0
                         if (
                             error == hidpp10_constants.ERROR.resource_error

lib/logitech_receiver/common.py
@@ -370,12 +370,12 @@ class NamedInts:
 
     __slots__ = ("__dict__", "_values", "_indexed", "_fallback", "_is_sorted")
 
-    def __init__(self, dict=None, **kwargs):
+    def __init__(self, dict_=None, **kwargs):
         def _readable_name(n):
             return n.replace("__", "/").replace("_", " ")
 
         # print (repr(kwargs))
-        elements = dict if dict else kwargs
+        elements = dict_ if dict_ else kwargs
         values = {k: NamedInt(v, _readable_name(k)) for (k, v) in elements.items()}
         self.__dict__ = values
         self._is_sorted = False
@@ -499,7 +499,7 @@ class NamedInts:
         return NamedInts(**self.__dict__, **other.__dict__)
 
     def __eq__(self, other):
-        return type(self) == type(other) and self._values == other._values
+        return isinstance(other, self.__class__) and self._values == other._values
 
 
 class UnsortedNamedInts(NamedInts):
@@ -548,7 +548,7 @@ class FirmwareInfo:
     kind: str
     name: str
     version: str
-    extras: str
+    extras: str | None
 
 
 class BatteryStatus(IntEnum):

commit 73ced3a7daab7e06d09440609a8b87aa7ba94a35

rules: Fix last_result not passed among chained rules
A previous refactor accidentally broke chained rules that rely on the
result chaining, so fix it.

With it fixed, some rule examples from docs/rules.md work properly
again, yay!

    ---
    - Feature: THUMB WHEEL
    - Rule: [ Modifiers: Control, Test: thumb_wheel_up, MouseScroll: [-2, 0] ]
    - Rule:
      - Modifiers: Control
      - Test: thumb_wheel_down
      - MouseScroll: [-2, 0]
    - Rule: [ Or: [ Test: thumb_wheel_up, Test: thumb_wheel_down ], MouseScroll: [-1, 0] ]
    ...
    ---
    - Feature: LOWRES WHEEL
    - Rule: [ Or: [ Test: lowres_wheel_up, Test: lowres_wheel_down ], MouseScroll: [0, 2] ]
    ...

Fixes: 46366b243012 ("Fix warnings from automatic code inspections")
Signed-off-by: Rongrong <i@rong.moe>

lib/logitech_receiver/diversion.py
@@ -481,14 +481,13 @@ class RuleComponent:
 
 
 def _evaluate(components, feature, notification: HIDPPNotification, device, result) -> Any:
-    res = True
     for component in components:
-        res = component.evaluate(feature, notification, device, result)
-        if not isinstance(component, Action) and res is None:
+        result = component.evaluate(feature, notification, device, result)
+        if not isinstance(component, Action) and result is None:
             return None
-        if isinstance(component, Condition) and not res:
-            return res
-    return res
+        if isinstance(component, Condition) and not result:
+            return result
+    return result
 
 
 class Rule(RuleComponent):